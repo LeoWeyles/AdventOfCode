359df6b276c1179e4541c3a2369d7204e5d012dc
LeoWeyles <96039666+LeoWeyles@users.noreply.github.com>
2022-12-02 19:05:36 -0500

    day 2, part 1

---
+++ 2022/day02/test_input.zig
@@ -0,0 +1,13 @@
+const io = @import("std").io;
+
+pub const data =
+	\\A Y
+	\\B X
+	\\C Z
+	;
+
+pub var stream = io.StreamSource {
+	.const_buffer = io.fixedBufferStream(data)
+};
+
+pub const part1_value: u32 = 15;
